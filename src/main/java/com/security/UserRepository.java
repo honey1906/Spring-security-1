a95855eeb110d5bf64be3e776d33ed384c0597f9
Honey Chandaly <fnuhoney@Honeys-MacBook-Pro.local>
2020-08-22 17:10:08 -0400

    first commit

---
+++ src/main/java/com/security/UserRepository.java
@@ -0,0 +1,10 @@
+package com.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	
+	User findByUsername(String username);
+
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Welcome page</title>
+</head>
+<body>
+Welcome...
+
+</body>
+
+</html>
